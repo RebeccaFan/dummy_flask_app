bdb3dd84d83c5c666ff882b66adac97a43fa6afc
Rebecca Fan <c17483124@mydit.ie>
2018-11-29 16:43:59 +0000

    Website V1

--- dummy_flask_app/templates/app.py
+++ dummy_flask_app/templates/app.py
@@ -3,9 +3,9 @@ from  flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/") 
+@app.route("/")
 def hello():
-    return render_template("hello.html")
+    return render_template("project.html")
 
 
 if __name__ == "__main__":
