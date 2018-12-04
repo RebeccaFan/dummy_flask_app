d7dc5fabf5ade73d36c156d508bc2478976d27ca
Katrzyna Kielbasa <katkielbasa@gmail.com>
2018-12-04 23:47:14 +0100

    -change images -some random tests with response...getting closer

--- dummy_flask_app/app.py
+++ dummy_flask_app/app.py
@@ -42,18 +42,29 @@ def book_flight():
     df = datetime.datetime.strptime(d,"%Y-%m-%d").strftime("%d/%m/%Y")
     noa = form_data['Passengers']
     queryUrl = flightquery(ff, ft , df, noa)
-    response = requests.get(queryUrl)
-    #jsonify(request.form.to_dict())
+    r = requests.get(queryUrl)
+    # r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    json_object = r.json()
+    # temp_k = float(json_object['main']['temp'])
+    #jsonify(content())
     # return jsonify(request.form.to_dict())
     # content2= (response.content).decode('utf-8')
     # content="".join(map(chr, response.content)).encode()
-    return response.content
-    # response.contecontent
+    print ( json_object )
+    if json_object['data'] == []:
+        print( "CHECK OTHER DATE OR LOCATION")
+    # ffrom = form_data['city_from']
+    # fto = form_data['city_to']
+    # date = float(json_object['main']['temp'])
+    # airline_logo = "https://images.kiwi.com/airlines/64x64/"+"LO"+".png"
+    return r.content
+    # render_template("bookFlight.html", fl)
+    # response.contejson
 #     return datetime.datetime.strptime(date,"%Y-%m-%d").strftime("%d/%m/%Y")
 
 def flightquery(flyFrom, flyTo, dateFrom, numberOfAdults):
     endpoint = "https://api.skypicker.com/flights?"
-    requestString=endpoint+"flyFrom="+flyFrom+"&to="+flyTo+"&dateFrom="+dateFrom+"&adults="+numberOfAdults+"&max_stopovers=0&partner=picky"
+    requestString=endpoint+"flyFrom="+flyFrom+"&to="+flyTo+"&dateFrom="+dateFrom+"&adults="+numberOfAdults+"&max_stopovers=0&partner=picky&limit=30&sort=price&asc=1"
     return requestString
 if __name__ == "__main__":
     app.run(debug=True)
