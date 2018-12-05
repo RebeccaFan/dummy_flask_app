8354a65a61bc5a215de7b2da2798166ba8b7dc4e
Katrzyna Kielbasa <katkielbasa@gmail.com>
2018-12-05 03:45:00 +0100

    add choose flight page and logic

--- dummy_flask_app/app.py
+++ dummy_flask_app/app.py
@@ -25,46 +25,44 @@ def budget():
 @app.route("/bookflight",methods=["POST"])
 def book_flight():
     form_data = request.form
-
-    # return "All OK"
-    # this line prints out the form to the browser
-    # payload = {
-    # 'flyFrom': form_data['city_from'], # default value is 'PRG'
-    # 'to': form_data['city_to'],
-    # 'dateFrom': form_data['dateFrom'],
-    # 'dateTo': arrow.utcnow().shift(weeks=+3).format('DD/MM/YYYY'),
-    # 'partner': 'picky' # default value is 'picky' use it for testing
-    # }
-
-    d = form_data['date_from']
+    df = form_data['date_from']
+    dt = form_data['date_to']
     ff = form_data['city_from']
     ft = form_data['city_to']
-    df = datetime.datetime.strptime(d,"%Y-%m-%d").strftime("%d/%m/%Y")
+    dff = datetime.datetime.strptime(df,"%Y-%m-%d").strftime("%d/%m/%Y")
+    dtf = datetime.datetime.strptime(dt,"%Y-%m-%d").strftime("%d/%m/%Y")
     noa = form_data['Passengers']
-    queryUrl = flightquery(ff, ft , df, noa)
+    queryUrl = flightquery(ff, ft , dff, dtf, noa)
     r = requests.get(queryUrl)
-    # r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    # print (queryUrl)
     json_object = r.json()
-    # temp_k = float(json_object['main']['temp'])
-    #jsonify(content())
-    # return jsonify(request.form.to_dict())
-    # content2= (response.content).decode('utf-8')
-    # content="".join(map(chr, response.content)).encode()
-    print ( json_object )
-    if json_object['data'] == []:
-        print( "CHECK OTHER DATE OR LOCATION")
-    # ffrom = form_data['city_from']
-    # fto = form_data['city_to']
-    # date = float(json_object['main']['temp'])
-    # airline_logo = "https://images.kiwi.com/airlines/64x64/"+"LO"+".png"
-    return r.content
-    # render_template("bookFlight.html", fl)
-    # response.contejson
-#     return datetime.datetime.strptime(date,"%Y-%m-%d").strftime("%d/%m/%Y")
+    endpointAirlines = 'https://api.skypicker.com/airlines'
+    resp = requests.get(endpointAirlines)
+    airlineList = resp.json()
+    # print(airlineList.content)
 
-def flightquery(flyFrom, flyTo, dateFrom, numberOfAdults):
+    if json_object['data'] == []:
+        return render_template("no_flights.html")
+    else:
+       flights = (json_object['data'])
+    # flights = list(map(lambda f: f.merge({'dTimeUTC': datetime.datetime.fromtimestamp(f['dTimeUTC']})), flights[0:6] ))
+    for flight in flights[0:6]:
+        flight['dTimeUTC'] = datetime.datetime.fromtimestamp(flight['dTimeUTC'])
+        flight['aTimeUTC'] = datetime.datetime.fromtimestamp(flight['aTimeUTC'])
+        airline_code = flight['airlines'][0]
+        
+        for airline in airlineList:
+            if airline["id"] == airline_code:
+                flight['airline_name'] = airline["name"]
+                break
+        
+        flight['airlines'][0] = "https://images.kiwi.com/airlines/64x64/"+flight['airlines'][0]+".png"
+        
+    return  render_template('bookFLights.html', flights=flights[0:6])
+
+def flightquery(flyFrom, flyTo, dateFrom, dateTo, numberOfAdults):
     endpoint = "https://api.skypicker.com/flights?"
-    requestString=endpoint+"flyFrom="+flyFrom+"&to="+flyTo+"&dateFrom="+dateFrom+"&adults="+numberOfAdults+"&max_stopovers=0&partner=picky&limit=30&sort=price&asc=1"
+    requestString=endpoint+"flyFrom="+flyFrom+"&to="+flyTo+"&dateFrom="+dateFrom+"&dateTo="+dateTo+"&adults="+numberOfAdults+"&max_stopovers=0&partner=picky&limit=30&sort=price&asc=1"
     return requestString
 if __name__ == "__main__":
     app.run(debug=True)
